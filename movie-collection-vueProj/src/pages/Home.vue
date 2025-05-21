9506b5a90eb9d5a0479f570251fdb62b4afed3bf
chen <levavi.chen@gmail.com>
2025-05-21 19:04:49 +0300

    add store

--- movie-collection-vueProj/src/pages/Home.vue
+++ movie-collection-vueProj/src/pages/Home.vue
@@ -1,3 +1,20 @@
 <template>
     <h1>Home</h1>
+    <p>{{ $store.state.counter }}</p>
+    <button @click="onIncrement">+1</button>
 </template>
+
+<script>
+export default {
+    methods: {
+        onIncrement() {
+            this.$store.commit({ type: 'increment' })
+        }
+    },
+    computed: {
+        counter() {
+            return this.$store.getters.counter
+        }
+    }
+}
+</script>
